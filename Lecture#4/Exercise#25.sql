617cf3425f5962f14de8983d45625983d6244943
Tamás Stefanovits <tamas.stefanovits@gmail.com>
2019-04-28 21:56:22 +0200

    Update Exercise#25.sql

--- Lecture#4/Exercise#25.sql
+++ Lecture#4/Exercise#25.sql
@@ -26,6 +26,23 @@ COMMIT;
 /* Join the accidents and the date_table tables and select the first 10 rows! */ 
 SELECT *
 FROM date_table
-  JOIN accidents ON date_table.accident_date = accidents.accident_date
+  JOIN accidents ON date_table.accident_date = accidents.accident_date 
+LIMIT 10;
+
+/* Alternative solution just to show one accident_date column in the joined tables.. */ 
+SELECT date_table.accident_date,
+       dow,
+       car_type,
+       city,
+       factory_id,
+       accident_id,
+       car_brand,
+       country,
+       reported,
+       weather,
+       time_of_the_day,
+       damage
+FROM date_table
+  JOIN accidents ON date_table.accident_date = accidents.accident_date 
 LIMIT 10;
 

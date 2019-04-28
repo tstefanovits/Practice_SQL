1cb51b1e91643b3ce25238ad02eeae91fb9d4c50
Tamás Stefanovits <tamas.stefanovits@gmail.com>
2019-04-28 23:58:12 +0200

    Create Exercise#29.sql

---
+++ Lecture#4/Exercise#29.sql
@@ -0,0 +1,12 @@
+/* Which country had the most accidents in which the damage was covered by insurance? */
+SELECT country,
+       COUNT(*),
+       covered_by_insurance.paid
+FROM accidents
+  FULL JOIN covered_by_insurance ON accidents.accident_id = covered_by_insurance.accident_id
+WHERE covered_by_insurance.paid = 'true'
+GROUP BY accidents.country,
+         covered_by_insurance.paid
+ORDER BY COUNT(*) DESC LIMIT 1;
+
+

d0d5be8b87a7c27a4028fa164f7fc8d24e5af4ce
Tamás Stefanovits <tamas.stefanovits@gmail.com>
2019-04-29 15:19:53 +0200

    Update Exercise#33.sql

--- Lecture#5/Exercise#33.sql
+++ Lecture#5/Exercise#33.sql
@@ -23,7 +23,7 @@ SELECT *
 FROM (SELECT city,
              COUNT(*)
       FROM accidents
-      JOIN covered_by_insurance ON accidents.accident_id = covered_by_insurance.accident_id
+        JOIN covered_by_insurance ON accidents.accident_id = covered_by_insurance.accident_id
       WHERE paid = 'true'
       GROUP BY city
       ORDER BY COUNT(*) DESC) AS covered;
@@ -34,10 +34,11 @@ FROM (SELECT city,
              paid,
              COUNT(*)
       FROM accidents
-      JOIN covered_by_insurance ON accidents.accident_id = covered_by_insurance.accident_id
+        JOIN covered_by_insurance ON accidents.accident_id = covered_by_insurance.accident_id
       WHERE paid = 'true'
-      OR paid = 'false'
-      GROUP BY city, paid
+      OR    paid = 'false'
+      GROUP BY city,
+               paid
       ORDER BY COUNT(*) DESC) AS covered;
 
 /* Distribution of the accidents what are covered by insurance. */ 
@@ -45,13 +46,13 @@ SELECT *
 FROM (SELECT city,
              COUNT(*)
       FROM accidents
-      JOIN covered_by_insurance ON accidents.accident_id = covered_by_insurance.accident_id
+        JOIN covered_by_insurance ON accidents.accident_id = covered_by_insurance.accident_id
       WHERE paid = 'true'
-      OR paid = 'false'
+      OR    paid = 'false'
       GROUP BY city
-      ORDER BY COUNT(*) DESC) AS covered;  
-      
-/* Distribution of cities by number of accidents. */   
+      ORDER BY COUNT(*) DESC) AS covered;
+
+/* Distribution of cities by number of accidents where reported = true. */ 
 SELECT city,
        COUNT(*)
 FROM accidents
@@ -59,6 +60,28 @@ WHERE reported = 'true'
 GROUP BY city
 ORDER BY COUNT(*) DESC;
 
-/* Which 3 cities have the highest proportion of reported accidents? */
+/* Distribution of cities by number of accidents where reported = false. */ 
+SELECT city,
+       COUNT(*)
+FROM accidents
+WHERE reported = 'false'
+GROUP BY city
+ORDER BY COUNT(*) DESC;
 
-/* Alternative solution. */
+/* Which 3 cities have the highest proportion of reported accidents? */ 
+
+/* Alternative solution. */ 
+SELECT reported.city,
+       reported.count_accidents::FLOAT/(reported.count_accidents + not_reported.count_accidents)::FLOAT AS ratio
+FROM (SELECT city,
+             COUNT(*) AS count_accidents
+      FROM accidents
+      WHERE reported = 'true'
+      GROUP BY city) AS reported
+  JOIN (SELECT city,
+               COUNT(*) AS count_accidents
+        FROM accidents
+        WHERE reported = 'false'
+        GROUP BY city) AS not_reported ON reported.city = not_reported.city
+ORDER BY ratio DESC LIMIT 3;
+

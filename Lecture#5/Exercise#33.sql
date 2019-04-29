b9a27052779ca1a22dcefddaf9d85d5dfceae7ff
Tamás Stefanovits <tamas.stefanovits@gmail.com>
2019-04-29 14:33:51 +0200

    Update Exercise#33.sql

--- Lecture#5/Exercise#33.sql
+++ Lecture#5/Exercise#33.sql
@@ -1,20 +1,32 @@
-/* Joining tables. */
+/* Joining tables. */ 
 SELECT *
 FROM accidents
-JOIN covered_by_insurance ON accidents.accident_id = covered_by_insurance.accident_id;
+  JOIN covered_by_insurance ON accidents.accident_id = covered_by_insurance.accident_id;
 
-/* Distribution of the accidents what are covered by insurance. */
+/* Distribution of the accidents what are covered by insurance. */ 
 SELECT *
 FROM accidents
-JOIN covered_by_insurance ON accidents.accident_id = covered_by_insurance.accident_id
+  JOIN covered_by_insurance ON accidents.accident_id = covered_by_insurance.accident_id
 WHERE paid = 'true';
 
-/* Distribution of the accidents what are covered by insurance in each country. */
-SELECT country, COUNT(*)
+/* Distribution of the accidents what are covered by insurance in each country. */ 
+SELECT country,
+       COUNT(*)
 FROM accidents
-JOIN covered_by_insurance ON accidents.accident_id = covered_by_insurance.accident_id
+  JOIN covered_by_insurance ON accidents.accident_id = covered_by_insurance.accident_id
 WHERE paid = 'true'
 GROUP BY country
 ORDER BY COUNT(*) DESC;
 
+/* Distribution of the accidents what are covered by insurance in each country. */ 
+SELECT *
+FROM (SELECT country,
+             COUNT(*)
+      FROM accidents
+      JOIN covered_by_insurance ON accidents.accident_id = covered_by_insurance.accident_id
+      WHERE paid = 'true'
+      GROUP BY country
+      ORDER BY country DESC) AS covered;
+
 /* Which 3 cities have the highest proportion of reported accidents? */
+

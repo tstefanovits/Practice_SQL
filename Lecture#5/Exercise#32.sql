24b2ed9b12e410cba0376fb77663e32ac378822e
Tamás Stefanovits <tamas.stefanovits@gmail.com>
2019-04-29 13:55:22 +0200

    Update Exercise#32.sql

--- Lecture#5/Exercise#32.sql
+++ Lecture#5/Exercise#32.sql
@@ -48,12 +48,54 @@ FROM (SELECT SUM(damage)
       ORDER BY SUM(damage) DESC) AS total;
      
 /* Total damage of all country. */
-SELECT SUM(total)
-FROM (SELECT SUM(damage) AS total
+SELECT SUM(total_dmg)
+FROM (SELECT SUM(damage) AS total_dmg
       FROM accidents
       GROUP BY country
       ORDER BY SUM(damage) DESC) AS total;
       
+/* Total 1% damage of all country. */      
+SELECT SUM(total_dmg) / 100 AS percent1
+FROM (SELECT SUM(damage) AS total_dmg
+      FROM accidents
+      GROUP BY country
+      ORDER BY SUM(damage) DESC) AS total;
+
+/* Total damage from each country. */
+SELECT *
+FROM (SELECT country, SUM(damage) 
+      FROM accidents
+      GROUP BY country
+      ORDER BY SUM(damage) DESC) AS total;
+
+/* Most damaged country damage. */     
+SELECT MAX(dmg) AS max_dmg
+FROM (SELECT country, 
+             SUM(damage) AS dmg 
+      FROM accidents
+      GROUP BY country
+      ORDER BY SUM(damage) DESC) AS total;
+
+/* Most damaged country damage / total damage. */     
+SELECT MAX(dmg) / SUM(total.dmg) AS percentage 
+FROM (SELECT country, 
+             SUM(damage) AS dmg 
+      FROM accidents
+      GROUP BY country
+      ORDER BY SUM(damage) DESC) AS total;
+
 /* Then take all these total-by-country damage values and calculate the ratio of the highest total damage cost (one country) to the total damage cost for all countries. 
 Express this ratio as a percentage! (You should compose this in one query, and get one number as the result!) */
-
+SELECT MAX(dmg) / SUM(total.dmg) * 100 AS percentage
+FROM (SELECT country, 
+             SUM(damage) AS dmg 
+      FROM accidents
+      GROUP BY country
+      ORDER BY SUM(damage) DESC) AS total;
+      
+/* Alternative solution. */
+SELECT MAX(damage_by_country.sum_damage) / SUM(damage_by_country.sum_damage)*100 AS percentage
+FROM (SELECT country,
+             SUM(damage) AS sum_damage
+      FROM accidents
+      GROUP BY country) AS damage_by_country;

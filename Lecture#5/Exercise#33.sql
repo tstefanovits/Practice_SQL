5e1baed15d705d1b237b4a93768a8a3fa901c9ba
Tamás Stefanovits <tamas.stefanovits@gmail.com>
2019-04-29 14:24:35 +0200

    Update Exercise#33.sql

--- Lecture#5/Exercise#33.sql
+++ Lecture#5/Exercise#33.sql
@@ -1,6 +1,20 @@
 /* Joining tables. */
 SELECT *
 FROM accidents
-JOIN covered_by_insurance ON accidents.accident_id = covered_by_insurance.accident_id ;
+JOIN covered_by_insurance ON accidents.accident_id = covered_by_insurance.accident_id;
+
+/* Distribution of the accidents what are covered by insurance. */
+SELECT *
+FROM accidents
+JOIN covered_by_insurance ON accidents.accident_id = covered_by_insurance.accident_id
+WHERE paid = 'true';
+
+/* Distribution of the accidents what are covered by insurance in each country. */
+SELECT country, COUNT(*)
+FROM accidents
+JOIN covered_by_insurance ON accidents.accident_id = covered_by_insurance.accident_id
+WHERE paid = 'true'
+GROUP BY country
+ORDER BY COUNT(*) DESC;
 
 /* Which 3 cities have the highest proportion of reported accidents? */

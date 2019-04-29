1bc62b4d5c3a5c1a84cdbc244de00212f6d666ab
Tamás Stefanovits <tamas.stefanovits@gmail.com>
2019-04-29 15:07:00 +0200

    Update Exercise#33.sql

--- Lecture#5/Exercise#33.sql
+++ Lecture#5/Exercise#33.sql
@@ -9,37 +9,56 @@ FROM accidents
   JOIN covered_by_insurance ON accidents.accident_id = covered_by_insurance.accident_id
 WHERE paid = 'true';
 
-/* Distribution of the accidents what are covered by insurance in each country. */ 
-SELECT country,
+/* Distribution of the accidents what are covered by insurance in each cities. */ 
+SELECT city,
        COUNT(*)
 FROM accidents
   JOIN covered_by_insurance ON accidents.accident_id = covered_by_insurance.accident_id
 WHERE paid = 'true'
-GROUP BY country
+GROUP BY city
 ORDER BY COUNT(*) DESC;
 
-/* Distribution of the accidents what are covered by insurance in each country. */ 
+/* Distribution of the accidents what are covered by insurance in each cities. */ 
 SELECT *
-FROM (SELECT country,
+FROM (SELECT city,
              COUNT(*)
       FROM accidents
       JOIN covered_by_insurance ON accidents.accident_id = covered_by_insurance.accident_id
       WHERE paid = 'true'
-      GROUP BY country
-      ORDER BY country DESC) AS covered;
+      GROUP BY city
+      ORDER BY COUNT(*) DESC) AS covered;
 
-/* Distribution of the accidents what are covered or not in each country. */ 
+/* Distribution of the accidents what are covered or not in each cities. */ 
 SELECT *
-FROM (SELECT country,
+FROM (SELECT city,
              paid,
              COUNT(*)
       FROM accidents
       JOIN covered_by_insurance ON accidents.accident_id = covered_by_insurance.accident_id
       WHERE paid = 'true'
       OR paid = 'false'
-      GROUP BY country, paid
-      ORDER BY country DESC) AS covered;
+      GROUP BY city, paid
+      ORDER BY COUNT(*) DESC) AS covered;
+
+/* Distribution of the accidents what are covered by insurance. */ 
+SELECT *
+FROM (SELECT city,
+             COUNT(*)
+      FROM accidents
+      JOIN covered_by_insurance ON accidents.accident_id = covered_by_insurance.accident_id
+      WHERE paid = 'true'
+      OR paid = 'false'
+      GROUP BY city
+      ORDER BY COUNT(*) DESC) AS covered;  
       
+/* Distribution of cities by number of accidents. */   
+SELECT city,
+       COUNT(*)
+FROM accidents
+WHERE reported = 'true'
+GROUP BY city
+ORDER BY COUNT(*) DESC;
 
 /* Which 3 cities have the highest proportion of reported accidents? */
 
+/* Alternative solution. */

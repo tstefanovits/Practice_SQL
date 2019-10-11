3677b208843311287d9f83f1cf579e51e28d687c
Tamás Stefanovits <tamas.stefanovits@gmail.com>
2019-10-11 10:50:13 +0200

    Data loaded to tables.

--- Vault #3/Case #1.sql
+++ Vault #3/Case #1.sql
@@ -1,21 +1,35 @@
 /* Creating abtest_users table. */ 
-CREATE TABLE abtest_users
+CREATE TABLE abtest_users 
 (
-   user_id  integer,
-   segment  text
+  user_id   INTEGER,
+  segment   TEXT
 );
 
 /* Creating abtest_purchases table. */ 
-CREATE TABLE abtest_purchases
+CREATE TABLE abtest_purchases 
 (
-   user_id integer,
-   purchase_id  text
+  user_id       INTEGER,
+  purchase_id   TEXT
 );
 
 /* Creating abtest_purchases table. */ 
-CREATE TABLE abtest_companies
+CREATE TABLE abtest_companies 
 (
-   user_id integer,
-   phone_type  text,
-   company_id  text
+  user_id      INTEGER,
+  phone_type   TEXT,
+  company_id   TEXT
 );
+
+/* Load data to abtest_users table. */ 
+COPY abtest_users
+FROM '/home/jovyan/work/Vault#3/abtest_users.csv' WITH CSV HEADER DELIMITER ',';
+
+/* Load data to abtest_purchases table. */ 
+COPY abtest_purchases
+FROM '/home/jovyan/work/Vault#3/abtest_purchases.csv' WITH CSV HEADER DELIMITER ',';
+
+/* Load data to abtest_companies table. */ 
+COPY abtest_companies
+FROM '/home/jovyan/work/Vault#3/abtest_companies.csv' WITH CSV HEADER DELIMITER ',';
+
+COMMIT;

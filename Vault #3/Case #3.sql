1ea1d7b415c027c074a6c872da907e42a3f41ed8
Tamás Stefanovits <tamas.stefanovits@gmail.com>
2019-10-23 22:35:17 +0200

    Task 1. solution added in Case #3.

--- Vault #3/Case #3.sql
+++ Vault #3/Case #3.sql
@@ -1,1 +1,10 @@
 /*  Introduction: In Case #3 I haven't got an actual dataset only hypothetical. So basically this file will contain my future solutions of a whiteboard interview. */
+
+/* Task 1: From which source does Dilan get the most first reads? */
+SELECT source,
+       COUNT(*)
+FROM first_read
+GROUP BY source
+ORDER BY source DESC;
+
+COMMIT;
